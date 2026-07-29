36e0d4f39035cbef1c8554d8958900767b7aeba8
abhishek-pixeldrift <abhishekkulkarni0000@gmail.com>
2026-07-29 19:05:29 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,45 @@
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+import os
+from google import genai
+from google.genai import types
+
+
+def generate():
+    client = genai.Client(
+        api_key=os.environ.get("AQ.Ab8RN6LMhhFe9-l1TjCM9hc7c-U3vlm48-M-TOa6_rurFWQk4A"),
+    )
+
+    model = "gemma-4-26b-a4b-it"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="""INSERT_INPUT_HERE"""),
+            ],
+        ),
+    ]
+    tools = [
+        types.Tool(googleSearch=types.GoogleSearch(
+        )),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        thinking_config=types.ThinkingConfig(
+            thinking_level="HIGH",
+        ),
+        tools=tools,
+    )
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        if text := chunk.text:
+            print(text, end="")
+
+if __name__ == "__main__":
+    generate()
+
+
